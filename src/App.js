131257d6735449c03afbe4bcf4f3cc8e9ab4630b
Bartek <bkotarski1@gmail.com>
2020-03-04 22:20:49 +0100

    Add sketch of Nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+import {Navigation} from "./components/Navigation";
+import {BrowserRouter as Router} from 'react-router-dom'
+const Application = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #181126;
+`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    render() {
+        return (
+            <Application>
+                <Router>
+                    <Navigation/>
+                </Router>
+            </Application>
+        )
+    }
+
 }
 
 export default App;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+import styled from 'styled-components'
+import Logo from '../../resources/img/logo.png'
+import {Link} from "react-router-dom";
+
+const NavBar = styled.div`
+width: 100%;
+height: 72px;
+`;
+
+const Nav = styled.nav`
+  float: left;
+  display: inline-block;
+  width: auto;
+  margin: 5px 5px;
+`;
+
+const Img = styled.img`
+    float: left;
+`;
+
+const Ul = styled.ul`
+  float: left;
+  list-style-type: none;
+  margin: 0;
+  width: auto;
+`;
+
+const Li = styled.li`
+  float: left;
+`;
+
+const ButtonSection = styled.div`
+  float: right;
+  padding: 19px;
+  display: inline-block;
+`;
+
+const LinkStyled = styled(Link)`
+  display: inline-block;
+  color: white;
+  text-align: center;
+  margin: 0 5px;
+  padding: 22px;
+  text-decoration: none;
+  font-size: 20px;
+`;
+
+const LoginButton = styled.button`
+  margin-right: 10px;
+  width: 200px;
+  height: 35px;
+  border: 3px dashed #41CE2A;
+  background-color: Transparent;
+  
+  font-weight: 900;
+  font-size: 20px;
+  color: white;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
+`;
+const RegisterButton = styled(LoginButton)`
+    border: 3px dashed #FF0000;
+`;
+
+
+
+export class Navigation extends React.Component {
+
+    render() {
+        return (
+            <NavBar>
+                <Nav>
+                    <Link to="/">
+                        <Img src={Logo} alt='logo'/>
+                    </Link>
+                    <Ul>
+                        <Li>
+                            <LinkStyled to="/map">Mapa</LinkStyled>
+                        </Li>
+                        <Li>
+                            <LinkStyled to="/contact">Kontakt</LinkStyled>
+                        </Li>
+                    </Ul>
+                </Nav>
+
+                <ButtonSection>
+                    <LoginButton>zaloguj</LoginButton>
+                    <RegisterButton>zarejestruj</RegisterButton>
+                </ButtonSection>
+
+            </NavBar>
+        );
+    }
+
+}
